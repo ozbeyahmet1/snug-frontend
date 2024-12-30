2d8893dd1b76cc295477a3f321d0f72492608da4
ozbeyahmet1 <ahmet1ozbey@gmail.com>
2024-12-30 08:46:19 +0300

    refactor: changes style of collection page

--- packages/collections/view.tsx
+++ packages/collections/view.tsx
@@ -15,13 +15,13 @@ export default function CollectionPageView() {
   return (
     <div className="min-h-[500px]">
       <TopBar slides={uiSchema.topBar} currentIndex={currentIndex} setCurrentIndex={setCurrentIndex} />
-      <div className="container mx-auto py-20">
+      <div className="container mx-auto py-10 lg:py-20 max-sm:px-5">
         {isLoading ? (
           <div className="text-lg text-gray-500 h-[500px] flex items-center w-full justify-center">
             <Image src={pillow} alt="" width={150} height={150} className="animate-rotate-step" />
           </div>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-10">
             {products?.map((product, index) => <FavoriteCard key={index} {...product} bgOpacity="bg-opacity-100" />)}
           </div>
         )}
--- packages/homepage/components/favoriteCard.tsx
+++ packages/homepage/components/favoriteCard.tsx
@@ -13,7 +13,7 @@ import Link from 'next/link';
 export function FavoriteCard({ title, price, image, href, bgOpacity }: Product & { bgOpacity: string }) {
   return (
     <Link className="flex-1 flex flex-col group cursor-pointer" href={`/product/${href}`}>
-      <div className={`bg-beige h-80 lg:h-96 flex items-center justify-center p-4 ${bgOpacity}`}>
+      <div className={`bg-beige h-80 lg:h-96 flex items-center justify-center p-4 animate-fadeIn ${bgOpacity}`}>
         <Image
           height={250}
           width={250}
